6238099595006401cb6ef3f74374b0807d13b6c8
Abhay Sastha S <abhaysastha.code@gmail.com>
2023-09-30 22:08:38 +0530

    improvised to compare with csv file of saved prof- -iles and its encodings

--- face_reco.py
+++ face_reco.py
@@ -1,24 +1,24 @@
 import cv2
+import csv
 import numpy as np
 import face_recognition
 
-#we consider image taken saved as image.jpg and the image to be compared with as original.jpg
-
-img_bgr = face_recognition.load_image_file('lib/original.jpg')
-#since face_recognition takes only bgr
-#we convert it to rgbw
-img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
-cv2.imshow('bgr',img_bgr)
-cv2.imshow('rgb',img_rgb)
-cv2.waitKey(0)
-
-#recognize the face in the image and save the encoding
-fixed_encode = face_recognition.face_encodings(img_rgb)[0]
-
-check = face_recognition.load_image_file('cache/captures/image.jpg')
-check = cv2.cvtColor(check, cv2.COLOR_BGR2RGB)
-check_encode = face_recognition.face_encodings(check)[0]
-check_result = face_recognition.compare_faces([fixed_encode], check_encode)
-
+#x is image file
+def compare_face(x):
+    check = face_recognition.load_image_file(x)
+    check = cv2.cvtColor(check, cv2.COLOR_BGR2RGB)
+    check_encode = face_recognition.face_encodings(check)[0]
+    
+    with open("saved_encodings.csv", mode='r', newline ='') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            temp_encode = row[1]
+            check_bool = face_recognition.compare_faces(temp_encode, check_encode)
+            if check_bool == True:
+                temp_name = row[0]
+                check_result = [check_bool, temp_name,temp_encode]
+                return check_result
+            else:
+                print("No match found")
 #check_result is a boolean value
 #can be used for future purposes
